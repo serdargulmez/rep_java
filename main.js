d6ec5d25de883f45bd398e7ed19d8a813b4e183f
serdargulmez <serdar_gulmez@outlook.com>
2025-04-25 17:38:39 +0300

    main-cm-02-Readme file and some example codes were added.

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 
 let tstString = "...starting program"
 console.log(tstString);
+
+let subStrings = tstString.split(" ");
+console.log(subStrings.length);
+console.log(subStrings.at(0));
+console.log(subStrings.at(1));
---
+++ readme.txt
@@ -0,0 +1,1 @@
+### This rep is used for checking java script syntaxes.
